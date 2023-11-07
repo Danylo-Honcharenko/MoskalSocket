16016a89c9fcdad72da5b9ed5a27ba7c6da375dc
Honcharenko Danylo Dmytrovych <danilo.gonch@gmail.com>
2023-11-07 12:56:01 +0200

    update PrintStream variable name

--- src/main/java/org/coursesjava/Logger.java
+++ src/main/java/org/coursesjava/Logger.java
@@ -6,11 +6,11 @@ import java.time.format.DateTimeFormatter;
 
 public class Logger {
     public static void write(String message, Exception exception) {
-        try (PrintStream ps = new PrintStream(new FileOutputStream(Path.DEFAULT.getPath() + "moskalSocket.log", true))) {
+        try (PrintStream writeLog = new PrintStream(new FileOutputStream(Path.DEFAULT.getPath() + "moskalSocket.log", true))) {
             String currentDate = DateTimeFormatter.ofPattern("hh:mm:ss, MM.dd.yyyy")
                     .format(LocalDateTime.now());
-            ps.println("[" + currentDate + "]" + " " + message);
-            exception.printStackTrace(ps);
+            writeLog.println("[" + currentDate + "]" + " " + message);
+            exception.printStackTrace(writeLog);
         } catch (IOException ex) {
             System.err.println("Error writing to log file: " + ex.getMessage());
         }
